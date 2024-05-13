102a9de71df17d0de77835217f593f9f0c4af729
Daniel Jary <danieljary90@googlemail.com>
2024-05-13 12:48:30 +0100

    Updated build script

--- holy-build-box_scripts/holy_build.sh
+++ holy-build-box_scripts/holy_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Save current path
+current_path=$(pwd)
+
 # Clear old project files and copy new ones in. 
 rm -rf /root/io/*
 mkdir /root/io              
@@ -8,3 +11,9 @@ cd /root/io
 
 # Execute holy build box script 'compile.sh' to compile portable code. 
 docker run -t -i --rm -v `pwd`:/io phusion/holy-build-box-64:latest bash /io/holy-build-box_scripts/compile.sh
+
+# Copy portable binary back to orignal path
+cp ELFie_portable $current_path
+
+# Clear files
+rm -rf /root/io/*
